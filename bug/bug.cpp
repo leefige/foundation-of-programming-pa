b858152da25b31a0be22698d69434467992b1d3c
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-11-09 10:22:48 +0800

    Update ignore.

---
+++ bug/bug.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char hello[13] = "Hello world!";
+    int zero = 3+5-8;
+    if (1 / zero == 0) {
+        cout << 1/zero << endl;
+    }
+    return 0;
+}
